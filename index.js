d32972b3458e112150f74f30a3087bf4d4214cbf
이한주 <gkswnehcl@gmail.com>
2024-09-09 11:25:01 +0900

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const todos = require('./api/todos');
+const todos = require('./api/todos.js');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
