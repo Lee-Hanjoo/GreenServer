14a6f209abbf10f08fa6d075f27eedd0c5e960b8
이한주 <gkswnehcl@gmail.com>
2024-09-09 10:31:38 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
-import './App.css';
-import React, { useEffect } from 'react';
-import axios from "axios";
+import React,{useEffect} from 'react'
 import store from './state/store';
 
-// 여기가 투두리스트 프론트단!!!!!********************
+import './assets/css/todos.scss';
+import Insert from './component/Insert';
+import List from './component/List';
+import Sort from './component/Sort';
+
+
 function App() {
 
-  // const data = store();     << {data:100}
-  const {data, dataCtrl} = store();   // << {100}
-
-
-  useEffect(()=>{
-    /*
-    1.get. delete << (url, option)
-
-    2.post,put << (url, body, option)
-    */
-    // axios.get("http://localhost:4000")
-    // axios.post("http://localhost:4000", {name:"test"})
-    // axios.delete("http://localhost:4000?id=1")
-    // .then(res=>{
-    //   console.log(res.data);
-    // })
-    console.log('hi');
-
-    dataCtrl({type:'post', data:{id:5,name:'콩순이'}});
-  },[])
-
   return (
-    <>
-
-    </>
+    <div className='todolist'>
+      <h2>todos</h2>
+      <div className='wrap'>
+        <Insert/>
+        <List/>
+        <Sort/>
+      </div>
+    </div>
   );
 }
 
 export default App;
+
+
+
+
