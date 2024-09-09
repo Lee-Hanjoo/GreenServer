c8d968ace2ebca4263ff73c405dae0d982774665
이한주 <gkswnehcl@gmail.com>
2024-09-09 10:17:58 +0900

    todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import axios from "axios";
+import store from './state/store';
 
+// 여기가 투두리스트 프론트단!!!!!********************
 function App() {
+
+  // const data = store();     << {data:100}
+  const {data, dataCtrl} = store();   // << {100}
+
+
+  useEffect(()=>{
+    /*
+    1.get. delete << (url, option)
+
+    2.post,put << (url, body, option)
+    */
+    // axios.get("http://localhost:4000")
+    // axios.post("http://localhost:4000", {name:"test"})
+    // axios.delete("http://localhost:4000?id=1")
+    // .then(res=>{
+    //   console.log(res.data);
+    // })
+    console.log('hi');
+
+    dataCtrl({type:'post', data:{id:5,name:'콩순이'}});
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // strict모드는 배포되면 사라짐. 두번씩 콘솔 찍히는거 이거 때문이지만, 아무 상관없음.
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
